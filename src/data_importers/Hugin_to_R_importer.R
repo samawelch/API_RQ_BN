cd51d6fc4591ccef62b011042261bbd7fcc7fd1d
sawelch-NIVA <saw@niva.no>
2023-01-23 15:47:21 +0100

    End of day commit

--- src/data_importers/Hugin_to_R_importer.R
+++ src/data_importers/Hugin_to_R_importer.R
@@ -1,32 +1,35 @@
 # Hugin has done its thing, so let's import the data
 Hugin_Data_Output <- read_csv(file = "Data/Hugin/Hugin_to_R_datafile.csv",
-                              show_col_types = FALSE) %>% 
-    # rename_with(cols = 8:15, ~str_remove(string = ., pattern = "\\[MEAN\\]")) %>% 
-    # rename_with(cols = 8:15, ~str_remove_all(string = ., pattern = "\\W"))
-    # Need to refactorise population scenarios
-    mutate(master_pop_scenario = fct_relevel(master_pop_scenario, c("Low", "Main", "High")),
-           master_WWT_scenario = fct_relevel(master_WWT_scenario, c("Current", "Secondary or better", "Best (tertiary)"))) %>% 
-    select(-c(6:11))
+                              show_col_types = FALSE)
 
-# Note the threshold used
-Hugin_Threshhold <-  unique(Hugin_Data_Output$master_RQ_threshold)
+# # Note the threshold used
+# Hugin_Threshhold <-  unique(Hugin_Data_Output$PRQ_threshold)
 
 # Hugin's many output interval columns need pivoting to tall data
 Hugin_Data_Output_Tall <- Hugin_Data_Output %>% 
-    pivot_longer(cols = 6:73,
+    pivot_longer(cols = 8:67,
                  names_to = "Risk_Bin_String",
                  values_to = "Probability") %>% 
     mutate(Risk_Type = str_remove(Risk_Bin_String, pattern = "_[^_]+$") %>% 
                str_remove(pattern = "P\\("),
            API_Name = str_extract(Risk_Bin_String, pattern = "(?<=\\_)([[:alpha:]]*?)(?=\\=)"),
            Risk_Bin = str_extract(Risk_Bin_String, pattern = "(?<==).[a-zA-Z0-9-]+"),
-           API_Name = fct_relevel(API_Name, c("Estradiol", "Ethinylestradiol", "Ciprofloxacin", 
-                                              "Diclofenac", "Ibuprofen", "Paracetamol", 
-                                              "Estrogens", "Antibiotics", "Painkillers", "AllAPI"))) %>% 
-    select(-Risk_Bin_String) %>% 
-    relocate(Probability, .after = last_col()) %>% 
-    # Replace "og" in county names with "&" for consistency
-    mutate(master_county = str_replace(master_county, pattern = " og ", replacement = " & "))
+           API_Name = fct_relevel(API_Name, c("estradiol", "ethinylestradiol", "ciprofloxacin", 
+                                              "diclofenac", "ibuprofen", "paracetamol", 
+                                              "Estrogens", "Painkillers", "AllAPI"))) %>% 
+    select(-Risk_Bin_String, -c(2:7)) %>% 
+    relocate(Probability, .after = last_col())
+
+scenario_number_labels = tibble(county = c("Rural", "Semi-Urban", "Urban")) %>% 
+    crossing(Year_and_Population_Growth = fct_inorder(c("2020 & None", "2050 & Low", 
+                                                        "2050 & Main", "2050 & High")),
+             WWT_Scenario = fct_inorder(c("Current", "Secondary or better", "Best (all tertiary)"))) %>% 
+    mutate(Scenario_Number = row_number()) %>% 
+    relocate(Scenario_Number, 1)
 
 # When was the dataset last updated?
 Hugin_Output_Last_Updated <- file.info("Data/Hugin/Hugin_to_R_datafile.csv")$mtime
+
+Hugin_Data_Output_Tall_Labelled <- Hugin_Data_Output_Tall %>% 
+    left_join(scenario_number_labels, by = c("scenario_number" = "Scenario_Number")) %>% 
+    relocate(county, Year_and_Population_Growth, WWT_Scenario, .after = scenario_number)
--- src/data_processing/R_to_Hugin.R
+++ src/data_processing/R_to_Hugin.R
@@ -1,20 +1,14 @@
 # Make data files for automated data input/output to Hugin
 
-All_RQ_Interval_Nodes <- append(paste0("API_RQ_FW_", str_to_title(analysed_APIs)), 
-                                c("SumRQ_Estrogens", "SumRQ_Antibiotics", 
-                                  "SumRQ_Painkillers","SumRQ_AllAPI"))
+All_RQ_Interval_Nodes <- append(paste0("API_RQ_FW_", tolower(analysed_APIs)), 
+                                c("RQ_FW_Estrogens", "RQ_FW_Painkillers","RQ_FW_AllAPI"))
 
-All_RQ_Boolean_Nodes <- c("PRQ_GT_Estrogens", "PRQ_GT_Antibiotics", "PRQ_GT_Painkillers", "PRQ_GT_AllAPI")
+All_RQ_Boolean_Nodes <- c("PRQ_GT_Estrogens", "PRQ_GT_Painkillers", "PRQ_GT_AllAPI")
 
-Hugin_Data_File <- tibble(master_pop_scenario = as_factor(c("Low", "Main", "High"))) %>% 
-    # Use vector recycling via crossing to set up the various scenario combinations easily
-    crossing(master_year = c("2020", "2050"),
-             master_WWT_scenario = c("Current", "Secondary or better", "Best (tertiary)"),
-             master_county = c("Whole Country", unlist(county_codes[2])),
-             master_RQ_threshold = c(100, 500, 1000)[2]) %>% 
+Hugin_Data_File <- tibble(scenario_number = 1:36) %>% 
     # Add API node value presets
     add_column(API_light_ethinylestradiol = "ethinylestradiol",
-               API_light_estriol = "estriol",
+               API_light_estradiol = "estradiol",
                API_medium_diclofenac = "diclofenac",
                API_medium_ciprofloxacin = "ciprofloxacin",
                API_heavy_paracetamol = "paracetamol",
